c29d058e5e9d387952b10e0ab7b56fab4002fbf2
Vladislav Klimenko <sunsingerus@gmail.com>
2019-06-10 15:13:27 +0300

    edit clickhouse minitest

--- scripts/full_cycle_minitest_clickhouse.sh
+++ scripts/full_cycle_minitest_clickhouse.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-$GOPATH/bin/tsbs_generate_data -format clickhouse -use-case cpu-only -scale 10 -seed 123 -file /tmp/bulk_data/clickhouse_data
+BULK_DATA_DIR="/tmp/bulk_data/"
 
-$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type lastpoint     -file /tmp/bulk_data/clickhouse_query_lastpoint
-$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type cpu-max-all-1 -file /tmp/bulk_data/clickhouse_query_cpu-max-all-1
-$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type high-cpu-1    -file /tmp/bulk_data/clickhouse_query_high-cpu-1
+# Ensure build data dir is in place
+mkdir -p "${BULK_DATA_DIR}"
 
-$GOPATH/bin/tsbs_load_clickhouse --db-name=benchmark --host=127.0.0.1 --workers=1 --file=/tmp/bulk_data/clickhouse_data
+# Generate data
+$GOPATH/bin/tsbs_generate_data -format clickhouse -use-case cpu-only -scale 10 -seed 123 -file "${BULK_DATA_DIR}/clickhouse_data"
 
-$GOPATH/bin/tsbs_run_queries_clickhouse --db-name=benchmark --hosts=127.0.0.1 --workers=1 --max-queries=100 --file=/tmp/bulk_data/clickhouse_query_lastpoint
-$GOPATH/bin/tsbs_run_queries_clickhouse --db-name=benchmark --hosts=127.0.0.1 --workers=1 --max-queries=100 --file=/tmp/bulk_data/clickhouse_query_cpu-max-all-1
-$GOPATH/bin/tsbs_run_queries_clickhouse --db-name=benchmark --hosts=127.0.0.1 --workers=1 --max-queries=100 --file=/tmp/bulk_data/clickhouse_query_high-cpu-1
+# Generate some queries
+$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type lastpoint     -file "${BULK_DATA_DIR}/clickhouse_query_lastpoint"
+$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type cpu-max-all-1 -file "${BULK_DATA_DIR}/clickhouse_query_cpu-max-all-1"
+$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type high-cpu-1    -file "${BULK_DATA_DIR}/clickhouse_query_high-cpu-1"
 
+# Load data generated earlier into ClickHouse
+$GOPATH/bin/tsbs_load_clickhouse --db-name=benchmark --host=127.0.0.1 --workers=1 --file="${BULK_DATA_DIR}/clickhouse_data"
 
+# Run some queries
+$GOPATH/bin/tsbs_run_queries_clickhouse --db-name=benchmark --hosts=127.0.0.1 --workers=1 --max-queries=100 --file="${BULK_DATA_DIR}/clickhouse_query_lastpoint"
+$GOPATH/bin/tsbs_run_queries_clickhouse --db-name=benchmark --hosts=127.0.0.1 --workers=1 --max-queries=100 --file="${BULK_DATA_DIR}/clickhouse_query_cpu-max-all-1"
+$GOPATH/bin/tsbs_run_queries_clickhouse --db-name=benchmark --hosts=127.0.0.1 --workers=1 --max-queries=100 --file="${BULK_DATA_DIR}/clickhouse_query_high-cpu-1"
